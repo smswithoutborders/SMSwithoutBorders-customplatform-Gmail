088987895a669099f06ed9d67bae8050efc73443
Sherlock Wisdom <wisdomnji@gmail.com>
2021-03-24 11:54:23 +0100

    update: it now works

---
+++ README.md
@@ -0,0 +1,5 @@
+# Notes
+- Developer needs to activate gmail for their account
+> https://console.cloud.google.com/apis/library/
+- Then download the needed credentials.json from there
+> _On your gmail console: add your email as a test email_
--- quickstart.py
+++ quickstart.py
@@ -6,7 +6,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/gmail.readonly']
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -25,7 +25,8 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            # creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server()
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
